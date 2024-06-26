f4687e42e29102a48b40f099e8f131e07dea157b
muabdi <91607604+TheBetterDark@users.noreply.github.com>
2024-06-26 23:18:56 +0100

    Fix ft_realloc

--- src/memory/ft_realloc.c
+++ src/memory/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:30:17 by muabdi            #+#    #+#             */
-/*   Updated: 2024/06/12 21:25:18 by muabdi           ###   ########.fr       */
+/*   Updated: 2024/06/26 23:18:43 by muabdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 	}
 	if (!new_size)
 		return (free(ptr), NULL);
-	new_ptr = malloc(old_size);
+	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (free(ptr), NULL);
 	if (old_size < new_size)
